c2767f9e5abd799119a791ad64036ab32f44dbfd
jilin <jilinxie1988@gmail.com>
2018-07-17 15:07:02 +0800

    fix: add updated_at(timestamp) to progress_record; add function to check if task in progress

--- cephutils/tools.py
+++ cephutils/tools.py
@@ -10,7 +10,7 @@ class StateRecorder(object):
     @classmethod
     def init_recorder(cls, progress_coll):
         cls.coll = progress_coll
-        cls.coll.ensure_index("created_at", expireAfterSeconds=24*3600)
+        cls.coll.ensure_index("updated_at", expireAfterSeconds=3*3600)
 
     def __init__(self):
         t = ''.join(random.choice(ascii_uppercase + digits) for _ in range(10))
@@ -18,8 +18,15 @@ class StateRecorder(object):
 
     def update_progress(self, state):
         self.coll.update({'token': self.token},
-                         {'$set': {'progress': state}},
+                         {'$set': {'progress': state,
+                                   'updated_at': int(time.time())}},
                          upsert=True)
+
+    def under_process(self, token):
+        res = self.coll.find_one({'token': token})
+        if res and res['progress'] == 'inprogress':
+            return True
+        return False
 
     def start(self):
         self.update_progress('inprogress')
@@ -31,7 +38,7 @@ class StateRecorder(object):
         self.update_progress('succeed')
 
     def invalid(self):
-        self.update_progress('invalid_md5')
+        self.update_progress('invalid')
 
     @classmethod
     def get_progress(cls, token):
